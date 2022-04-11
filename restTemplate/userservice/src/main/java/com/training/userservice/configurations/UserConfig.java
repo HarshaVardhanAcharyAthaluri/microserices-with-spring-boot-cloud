7ab3e3a8cafae0175d4fc41e744b65a2c79625de
HarshaVardhan <athaluri.harsha@gmail.com>
2022-04-11 10:48:13 +0530

    rest template

---
+++ restTemplate/userservice/src/main/java/com/training/userservice/configurations/UserConfig.java
@@ -0,0 +1,17 @@
+package com.training.userservice.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class UserConfig {
+
+	@Bean
+	public RestTemplate initRestTemplat() {
+		return new RestTemplate();
+	}
+	
+	
+	
+}
