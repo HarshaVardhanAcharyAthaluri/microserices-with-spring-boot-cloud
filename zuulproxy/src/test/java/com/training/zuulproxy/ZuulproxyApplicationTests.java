0513d965746ec0acc2f5d63613219523bb253620
HarshaVardhan <athaluri.harsha@gmail.com>
2022-04-14 10:37:28 +0530

    zuul proxy added

---
+++ zuulproxy/src/test/java/com/training/zuulproxy/ZuulproxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.zuulproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulproxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
